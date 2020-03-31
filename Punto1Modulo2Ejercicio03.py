e946afae218616fab894281a7e864803bb50f264
MicaelaTapia <micaelaloa@gmail.com>
2020-03-31 11:08:50 -0300

    Terminado

---
+++ Punto1Modulo2Ejercicio03.py
@@ -0,0 +1,7 @@
+millas = int(input("Ingresar millas: "))
+KM = 1.60935
+print("De millas a KM:", KM * millas)
+
+pulgadas = int(input("Ingrese pulgadas: "))
+cm = 2.534
+print("De pulgadas a cm:", pulgadas * cm)
