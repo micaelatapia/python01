d7189e9752894a0b24270e89395bdbbbee19c441
MicaelaTapia <micaelaloa@gmail.com>
2020-03-31 11:09:50 -0300

    Terminado

--- Punto1Modulo2Ejercicio01.py
+++ Punto1Modulo2Ejercicio01.py
@@ -1,4 +1,4 @@
-horasTrabajadas = "Ingresar cantidad de horas trabajadas; ")
-valorHora = "Ingresar valor de la hora: ")
+horasTrabajadas = int(input("Ingresar cantidad de horas trabajadas; "))
+valorHora = int(input("Ingresar valor de la hora: "))
 
 print("El resultado es :", horasTrabajadas * valorHora)
