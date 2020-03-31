d2a2fa98cb101133941c1cbd949db2cc678a1270
MicaelaTapia <micaelaloa@gmail.com>
2020-03-31 09:35:42 -0300

    Terminado

--- Ejercicio04.py
+++ Ejercicio04.py
@@ -5,8 +5,8 @@ Diseñar un programa que permita ingresar los datos y calcular:
     a) Cantidad de hombres mayores a 45 años.
     b) Cantidad de mujeres encuestadas.
     c) Promedio de edad de las personas para cada tipo de estudio.
- ) Porcentaje de mujeres con estudios terciarios.
-e) Porcentaje de hombres mayores a 30 años con estudios terciarios.
+    d) Porcentaje de mujeres con estudios terciarios.
+    e) Porcentaje de hombres mayores a 30 años con estudios terciarios.
 f) Promedio de edad de las mujeres mayores a 40 años con estudios secundarios."""
 
 cont_45 = 0
@@ -18,6 +18,9 @@ cont_secundaria = 0
 acum_tersario = 0
 cont_tersario = 0
 contMujeresTer = 0
+contHombresTer = 0
+contHombres = 0
+acumEdadMujeres = 0
 
 sexo = int(input("Ingresar sexo, 1.F o 2.M: "))
 edad = int(input("Ingresar edad: "))
@@ -26,6 +29,12 @@ estudios = int(input("Estudios 1.primarios 2.secundarios o 3.terciaros: "))
 while edad != 0 and estudios != 0:
     if sexo == 2 and edad > 45:
         cont_45 = cont_45 + 1
+
+    if sexo == 2:
+        contHombres = contHombres + 1
+
+    if sexo == 2 and edad >= 30 and estudios == 3:
+        contHombresTer = contHombresTer + 1
 
     if sexo == 1:
         cont_Mujeres = cont_Mujeres + 1
@@ -46,6 +55,9 @@ while edad != 0 and estudios != 0:
         if estudios == 3:
             contMujeresTer = contMujeresTer + 1
 
+    if sexo == 1 and edad >= 40 and estudios == 2:
+        acumEdadMujeres = acumEdadMujeres + edad
+
     sexo = int(input("Ingresar sexo, 1.F o 2.M: "))
     edad = int(input("Ingresar edad: "))
     estudios = int(input("Estudios 1.primarios 2.secundarios o 3.terciaros: "))
@@ -56,4 +68,5 @@ print("Promedio de estudio primario:", acum_primaria / cont_primaria, "%")
 print("Promedio de secundario:", acum_secundaria / cont_secundaria, "%")
 print("Promedio de estudios terciarios:", acum_tersario / cont_tersario, "%")
 print("Porcentaje de mujeres con estudios terciarios:", contMujeresTer / cont_Mujeres * 100, "%")
-print("Porcentaje de hombres mayores a 30 años con estudios terciarios.")
+print("Porcentaje de hombres mayores a 30 años con estudios terciarios.", contHombresTer / contHombres * 100, "%")
+print("Promedio de edad de mujeres mayores a 40 años con estudios secundarios:", acumEdadMujeres / cont_Mujeres, "%")
