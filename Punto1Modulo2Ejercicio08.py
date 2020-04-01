45db2568eb10be9d3b7a1e339daab296873a6dbf
MicaelaTapia <micaelaloa@gmail.com>
2020-04-01 14:34:22 -0300

    Terminado

---
+++ Punto1Modulo2Ejercicio08.py
@@ -0,0 +1,7 @@
+PrimerCociente = int(input("Ingresar cociente: "))
+SegundoCociente = int(input("Ingresar otro cociente: "))
+
+if SegundoCociente == 0:
+    print("No de puede realizar")
+
+print("El resultado es: ", PrimerCociente / SegundoCociente)
