c05a575911ac3416ef07d3b3ad649262dcaa658d
MicaelaTapia <micaelaloa@gmail.com>
2020-04-01 14:10:28 -0300

    Terminado

---
+++ Punto1Modulo2Ejercicio04.py
@@ -0,0 +1,7 @@
+hipotenusa1 = int(input("Ingresar la primera hipotenusa: "))
+hipotenusa1 = int(input("Ingresar la segunda hipotenusa: "))
+hipotenusa3 = int(input("Ingresar la tercera hipotenusa: "))
+
+sumaTotal = hipotenusa1 + hipotenusa1 + hipotenusa3
+
+print("El resultado es:", sumaTotal)
