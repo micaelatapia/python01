5457b2c662c58c09943d5c9c2e910d4f8104ba92
MicaelaTapia <micaelaloa@gmail.com>
2020-04-02 14:46:30 -0300

    Prueba

---
+++ Punto1Modulo3Ejercicio2.py
@@ -0,0 +1,20 @@
+NumeroSemana = int(input("Ingrese numero de semana: "))
+GastoSemanal = int(input("Ingrese gasto semanal: "))
+
+if NumeroSemana < 5:
+    print("El gasto semanal es de: $", GastoSemanal * 7 / 100)
+
+while NumeroSemana <= 0 or NumeroSemana > 5:
+    print("Error, ingrese un numero de semana correcto")
+    NumeroSemana = int(input("Ingrese numero de semana: "))
+    GastoSemanal = int(input("Ingrese gasto semanal: "))
+
+    if NumeroSemana < 5:
+        print("El gasto semanal es de: $", GastoSemanal * 7 / 100)
+
+while NumeroSemana <= 4:
+    NumeroSemana = int(input("Ingrese numero de semana: "))
+    GastoSemanal = int(input("Ingrese gasto semanal: "))
+
+    if NumeroSemana < 5:
+         print("El gasto semanal es de: $", GastoSemanal * 7 / 100
