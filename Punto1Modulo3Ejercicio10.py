4f0240c22e7430bca57e58160727f764364e957d
MicaelaTapia <micaelaloa@gmail.com>
2020-04-05 14:10:26 -0300

    Prueba

---
+++ Punto1Modulo3Ejercicio10.py
@@ -0,0 +1,12 @@
+
+n = int(input("Ingresar un numero: "))
+fact = 0
+
+if n >= 0:
+    for I in [n]:
+        fact = fact + I
+        print("El resultado es:", fact)
+    else:
+        print("Operacion finalizada")
+
+
