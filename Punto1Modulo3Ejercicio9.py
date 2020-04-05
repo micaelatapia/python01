99d0c6696af2d1d4adce59361afec2175b02328c
MicaelaTapia <micaelaloa@gmail.com>
2020-04-05 13:24:53 -0300

    Terminado

---
+++ Punto1Modulo3Ejercicio9.py
@@ -0,0 +1,21 @@
+
+cont = 0
+
+
+temperaturas = int(input("Ingrese temperaturas: "))
+max = temperaturas
+min = temperaturas
+
+while cont < 5:
+    cont = cont + 1
+
+
+    if temperaturas > max:
+        max = temperaturas
+
+    if temperaturas < min:
+        min = temperaturas
+
+    temperaturas = int(input("Ingrese temperaturas: "))
+
+print("La temperatura minima es:", min, "y la maxima:", max)
