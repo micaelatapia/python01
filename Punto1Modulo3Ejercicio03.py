801f326f5c9dbcd240899aea902f3266d131729c
MicaelaTapia <micaelaloa@gmail.com>
2020-04-06 10:34:06 -0300

    Terminado

--- Punto1Modulo3Ejercicio03.py
+++ Punto1Modulo3Ejercicio03.py
@@ -7,4 +7,4 @@ while num != 0:
         cont = cont + 1
     num = int(input("Ingrese numero: "))
 
-print("Cantidad de numeros ingresados: ", cont)
+print("Cantidad de numeros positivos ingresados: ", cont)
