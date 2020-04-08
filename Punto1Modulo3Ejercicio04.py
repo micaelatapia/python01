b37d06a4159b3b71457bbcb93f7c38c26c6f957f
MicaelaTapia <micaelaloa@gmail.com>
2020-04-08 18:59:20 -0300

    Prueba

--- Punto1Modulo3Ejercicio04.py
+++ Punto1Modulo3Ejercicio04.py
@@ -1,10 +1,41 @@
-cont = 0
+"""4) Al terminar un día en un colegio secundario se hace una estadística de faltas sabiendo
+de cada curso:
+ Curso (1-5)
+ Presentes
+ Ausentes
+Calcular
+ Por cada curso el porcentaje de presentes sobre el total
+ Cantidad de ausentes en el colegio
+ Curso con mayor cantidad de ausente"""
 
-contp = 0
-acump = 0
+ausentes = 0
+contA = 0
+acumA = 0
 
-conta = 0
-acuma = 0
+curso1 = print("Curso 1")
+curso2 = print("Curso 2")
+curso3 = print("Curso 3")
+curso4 = print("Curso 4")
+curso5 = print("Curso 5")
+
+
+print("Ingresar presentes de cada curso:")
+curso1p = int(input("Curso 1: "))
+curso2p = int(input("Curso 2: "))
+curso3p = int(input("Curso 3: "))
+curso4p = int(input("Curso 4: "))
+curso5p = int(input("Curso 5: "))
+totalP = curso1p + curso2p + curso3p + curso4p + curso5p
+
+while contA < 5:
+    contA = contA + 1
+    acumA = acumA + ausentes
+    ausentes = int(input("Ingresar ausentes de cada curso: "))
+
+
+
+print("Porcentaje de presentes en el curso 1:", curso1p / (totalP + acumA) * 100)
+
 
 
 
