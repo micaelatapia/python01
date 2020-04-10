e69ab105b8a4fe3dd18bfae70b80864e8c296708
MicaelaTapia <micaelaloa@gmail.com>
2020-04-10 15:45:26 -0300

    prueba

--- Punto1Modulo3Ejercicio2.py
+++ Punto1Modulo3Ejercicio2.py
@@ -1,20 +1,15 @@
+"""2) Calcular el promedio semanal de gastos en un mes, ingresando como datos:
+ Semana número
+ Gasto semanal
+El proceso termina cuando “semana número” es igual a 5"""
+cont = 0
+
+
+
 NumeroSemana = int(input("Ingrese numero de semana: "))
 GastoSemanal = int(input("Ingrese gasto semanal: "))
-
-if NumeroSemana < 5:
-    print("El gasto semanal es de: $", GastoSemanal * 7 / 100)
 
 while NumeroSemana <= 0 or NumeroSemana > 5:
     print("Error, ingrese un numero de semana correcto")
     NumeroSemana = int(input("Ingrese numero de semana: "))
     GastoSemanal = int(input("Ingrese gasto semanal: "))
-
-    if NumeroSemana < 5:
-        print("El gasto semanal es de: $", GastoSemanal * 7 / 100)
-
-while NumeroSemana <= 4:
-    NumeroSemana = int(input("Ingrese numero de semana: "))
-    GastoSemanal = int(input("Ingrese gasto semanal: "))
-
-    if NumeroSemana < 5:
-        print("El gasto semanal es de: $", GastoSemanal * 7 / 100)
