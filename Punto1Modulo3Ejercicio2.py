1f7f62bccbeaa7384bdd6ecb5f21942e93de4963
MicaelaTapia <micaelaloa@gmail.com>
2020-04-13 23:29:24 -0300

    prueba

--- Punto1Modulo3Ejercicio2.py
+++ Punto1Modulo3Ejercicio2.py
@@ -9,7 +9,9 @@ cont = 0
 NumeroSemana = int(input("Ingrese numero de semana: "))
 GastoSemanal = int(input("Ingrese gasto semanal: "))
 
+
 while NumeroSemana <= 0 or NumeroSemana > 5:
+    cont = cont + 1
     print("Error, ingrese un numero de semana correcto")
     NumeroSemana = int(input("Ingrese numero de semana: "))
     GastoSemanal = int(input("Ingrese gasto semanal: "))
